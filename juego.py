c5a17add3a5b46bc146e769cdcbbecb3e69dab74
Dalvarezsaravia <dalvarezsaravia@uc.cl>
2025-08-28 10:40:56 -0400

    Menu

---
+++ juego.py
@@ -0,0 +1,11 @@
+print("Bienvenido a DCCorta")
+
+
+print("-" * 26 + " MENU " + "-"*26)
+print("[1] Papel")
+print("[2] Piedra")
+print("[3] Tijeras")
+print("[4] Lagartija")
+print("[5] spock \n")
+
+jugador_1 = input("Jugador 1:")
